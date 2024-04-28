31bc15261d5e8bfc863e25ca5fe1e482f0f10d48
daffamuliya <daffamuliya15@gmail.com>
2024-04-29 01:41:41 +0700

    update

--- client/src/pages/User/Seller/PesananSeller.js
+++ client/src/pages/User/Seller/PesananSeller.js
@@ -1,95 +1,83 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SellerNavbar from '../../../components/SellerNavbar';
 import Card from 'react-bootstrap/Card';
 import SidebarSeller from '../../../components/SidebarSeller';
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import Table from 'react-bootstrap/Table';
-
+import axios from 'axios';
 import CardBody from 'react-bootstrap/esm/CardBody';
 
-export default class Landing extends Component {
-  render() {
-    return (
-      <div>
-        <SellerNavbar />
-        <div className="main">
-          <SidebarSeller />
-          <div className="container mt-5">
-            <MDBRow className="mt-5">
-              <h5 style={{ color: '#A08336' }}>Pesanan</h5>
-              <MDBRow className="mt-3 justify-content-center">
-                <MDBCol md={12} xs={2} className="ms-auto">
-                  <Card>
-                    <CardBody>
-                      {' '}
-                      <div class="row">
-                        <Table responsive>
-                          <thead>
-                            <tr>
-                              <th>Order ID</th>
-                              <th>Produk</th>
-                              <th>Qty</th>
-                              <th>Tanggal</th>
-                              <th>Nama Kustomer</th>
-                              <th>Total</th>
-                              <th>Status</th>
-                              <th>Aksi</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                              <td>25 Januari 2022</td>
-                              <td>25 Januari 2022</td>
-                              <td>Otto</td>
-                              <td>
-                                <i class="bi bi-trash-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-pencil-square" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-eye-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                              <td>25 Januari 2022</td>
-                              <td>25 Januari 2022</td>
-                              <td>Otto</td>
-                              <td>
-                                <i class="bi bi-trash-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-pencil-square" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-eye-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                              <td>25 Januari 2022</td>
-                              <td>25 Januari 2022</td>
-                              <td>Otto</td>
-                              <td>
-                                <i class="bi bi-trash-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-pencil-square" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                                <i class="bi bi-eye-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </div>
-                    </CardBody>
-                  </Card>
-                </MDBCol>
-              </MDBRow>
+const PesananSeller = () => {
+  const [transaksi, setTransaksi] = useState([]);
+  useEffect(() => {
+    getTransaksi();
+  }, []);
+
+  const getTransaksi = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/marketplace/transaksi');
+      setTransaksi(response.data.data);
+    } catch (error) {
+      console.error('Error fetching blogs:', error);
+    }
+  };
+  return (
+    <div>
+      <SellerNavbar />
+      <div className="main">
+        <SidebarSeller />
+        <div className="container mt-5">
+          <MDBRow className="mt-5">
+            <h5 style={{ color: '#A08336' }}>Pesanan</h5>
+            <MDBRow className="mt-3 justify-content-center">
+              <MDBCol md={12} xs={2} className="ms-auto">
+                <Card>
+                  <CardBody>
+                    {' '}
+                    <div class="row">
+                      <Table responsive>
+                        <thead>
+                          <tr>
+                            <th>Order ID</th>
+                            <th>Product</th>
+                            <th>Date</th>
+                            <th>Customer</th>
+                            <th>Total</th>
+                            <th>Payment</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {Array.isArray(transaksi) &&
+                            transaksi.map((item) => (
+                              <tr>
+                                <td>{item.id}</td>
+                                <td>{item.produk.nama}</td>
+                                <td>{item.tanggal_pesan}</td>
+                                <td>{item.user.name}</td>
+                                <td>Rp {item.total}</td>
+                                <td>{item.payment}</td>
+                                <td>{item.status}</td>
+                                <td>
+                                  <i class="bi bi-trash-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>
+                                  <i class="bi bi-pencil-square" style={{ color: '#A08336', paddingRight: '10px' }}></i>
+                                  <i class="bi bi-eye-fill" style={{ color: '#A08336', paddingRight: '10px' }}></i>{' '}
+                                </td>
+                              </tr>
+                            ))}
+                        </tbody>
+                      </Table>
+                    </div>
+                  </CardBody>
+                </Card>
+              </MDBCol>
             </MDBRow>
-          </div>
+          </MDBRow>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default PesananSeller;
