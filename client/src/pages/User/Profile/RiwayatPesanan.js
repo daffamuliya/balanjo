2f7e83d48f37a1affae743263fb93bce3f087cd7
daffamuliya <daffamuliya15@gmail.com>
2024-04-29 01:57:46 +0700

    user forum and blog

--- client/src/pages/User/Profile/RiwayatPesanan.js
+++ client/src/pages/User/Profile/RiwayatPesanan.js
@@ -1,65 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserNavbar from '../../../components/UserNavbar';
 import Card from 'react-bootstrap/Card';
 import SidebarAkun from '../../../components/SidebarAkun';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-export default class Landing extends Component {
-  render() {
-    return (
-      <div>
-        <UserNavbar />
-        <div className="main">
-          <SidebarAkun />
-          <div className="container mt-5">
-            <Card>
-              <Card.Body>
-                <section className="blog">
-                  <div class="row">
-                    <h2 className="title" style={{ fontWeight: 'bold' }}>
-                      Riwayat Pesanan
-                    </h2>
-                    <Table responsive>
-                      <thead>
-                        <tr>
-                          <th>Order ID</th>
-                          <th>Produk</th>
-                          <th>Tanggal</th>
-                          <th>Status</th>
-                          <th>Price</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Mark</td>
-                          <td>Otto</td>
-                          <td>@mdo</td>
-                          <td>25 Januari 2022</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Jacob</td>
-                          <td>Thornton</td>
-                          <td>@fat</td>
-                          <td>25 Januari 2022</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>Larry the Bird</td>
-                          <td>Mahmudi</td>
-                          <td>@twitter</td>
-                          <td>25 Januari 2022</td>
-                        </tr>
-                      </tbody>
-                    </Table>
-                  </div>
-                </section>
-              </Card.Body>
-            </Card>
-          </div>
+const RiwayatPesanan = () => {
+  const [transaksi, setTransaksi] = useState([]);
+  useEffect(() => {
+    getTransaksi();
+  }, []);
+
+  const getTransaksi = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/marketplace/transaksi');
+      setTransaksi(response.data.data);
+    } catch (error) {
+      console.error('Error fetching blogs:', error);
+    }
+  };
+  return (
+    <div>
+      <UserNavbar />
+      <div className="main">
+        <SidebarAkun />
+        <div className="container mt-5">
+          <Card>
+            <Card.Body>
+              <section className="blog">
+                <div class="row">
+                  <h2 className="title" style={{ fontWeight: 'bold' }}>
+                    Riwayat Pesanan
+                  </h2>
+                  <Table responsive>
+                    <thead>
+                      <tr>
+                        <th>Order ID</th>
+                        <th>Product</th>
+                        <th>Date</th>
+                        <th>Total</th>
+                        <th>Payment</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {Array.isArray(transaksi) &&
+                        transaksi.map((item) => (
+                          <tr>
+                            <td>{item.id}</td>
+                            <td>{item.produk.nama}</td>
+                            <td>{item.tanggal_pesan}</td>
+                            <td>Rp {item.total}</td>
+                            <td>{item.payment}</td>
+                            <td>{item.status}</td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </Table>
+                </div>
+              </section>
+            </Card.Body>
+          </Card>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default RiwayatPesanan;
