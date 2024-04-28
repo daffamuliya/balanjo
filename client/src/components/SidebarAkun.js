bb6204f5ae84278c40c3742a9d0160d2b2b740df
daffamuliya <daffamuliya15@gmail.com>
2024-04-29 02:07:46 +0700

    update

--- client/src/components/SidebarAkun.js
+++ client/src/components/SidebarAkun.js
@@ -1,7 +1,20 @@
 import SidebarItem from './SidebarItem';
 import items from '../data/sidebarakun.json';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Sidebar() {
+const SidebarAkun = () => {
+  const [user, setUser] = useState([]);
+  const user_id = '2';
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const response = await axios.get(`http://localhost:3000/daftaruser/${user_id}`);
+    setUser(response.data.items);
+    console.log(response.data.items);
+  };
   return (
     <div className="sidebar">
       <img src="/img/sofia.png" width="100" height="100" className="d-inline-block align-top" style={{ marginLeft: '70px', marginBottom: '25px' }} alt="React Bootstrap logo" />
@@ -10,4 +23,6 @@ export default function Sidebar() {
       ))}
     </div>
   );
-}
+};
+
+export default SidebarAkun;
--- client/src/pages/User/Profile/AkunDetails.js
+++ client/src/pages/User/Profile/AkunDetails.js
@@ -1,81 +1,94 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserNavbar from '../../../components/UserNavbar';
 import Card from 'react-bootstrap/Card';
 import SidebarAkun from '../../../components/SidebarAkun';
+import axios from 'axios';
 
-export default class Landing extends Component {
-  render() {
-    return (
-      <div>
-        <UserNavbar />
-        <div className="main">
-          <SidebarAkun />
-          <div className="container mt-5 ">
-            <Card>
-              <Card.Body>
-                <section className="blog">
-                  <div class="row">
-                    <h2 className="title" style={{ fontWeight: 'bold' }}>
-                      Account Details
-                    </h2>
-                    <div class="col-12 mb-4 mt-4">
-                      <form>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            FULL NAME *
-                          </label>
-                          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Full Name" aria-describedby="emailHelp" />
-                        </div>
+const AkunDetails = () => {
+  const [user, setUser] = useState([]);
+  const user_id = '2';
+  useEffect(() => {
+    getUser();
+  }, []);
 
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            USERNAME *
-                          </label>
-                          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="User Name" aria-describedby="emailHelp" />
-                        </div>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            EMAIL *
-                          </label>
-                          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email" aria-describedby="emailHelp" />
-                        </div>
-                      </form>
-                    </div>
-                    <h2 className="title" style={{ fontWeight: 'bold' }}>
-                      Password
-                    </h2>
-                    <div class="col-12 mb-4 mt-4">
-                      <form>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            OLD PASSWORD
-                          </label>
-                          <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Old Password" aria-describedby="emailHelp" />
-                        </div>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            NEW PASSWORD
-                          </label>
-                          <input type="password" class="form-control" id="exampleInputEmail1" placeholder="New Password" aria-describedby="emailHelp" />
-                        </div>
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">
-                            REPEAT NEW PASSWORD
-                          </label>
-                          <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Repeat New Password" aria-describedby="emailHelp" />
-                        </div>
-                        <button type="submit" class="btn btn-primary" style={{ backgroundColor: '#A08336', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', textAlign: 'center', border: 'black', float: 'right' }}>
-                          Save Changes
-                        </button>
-                      </form>
-                    </div>
+  const getUser = async () => {
+    const response = await axios.get(`http://localhost:3000/daftaruser/${user_id}`);
+    setUser(response.data.items);
+    console.log(response.data.items);
+  };
+
+  return (
+    <div>
+      <UserNavbar />
+      <div className="main">
+        <SidebarAkun />
+        <div className="container mt-5 ">
+          <Card>
+            <Card.Body>
+              <section className="blog">
+                <div class="row">
+                  <h2 className="title" style={{ fontWeight: 'bold' }}>
+                    Account Details
+                  </h2>
+                  <div class="col-12 mb-4 mt-4">
+                    <form>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          FULL NAME *
+                        </label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder={user.name} aria-describedby="emailHelp" disabled />
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          USERNAME *
+                        </label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder={user.username} aria-describedby="emailHelp" disabled />
+                      </div>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          EMAIL *
+                        </label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder={user.email} aria-describedby="emailHelp" disabled />
+                      </div>
+                    </form>
                   </div>
-                </section>
-              </Card.Body>
-            </Card>
-          </div>
+                  <h2 className="title" style={{ fontWeight: 'bold' }}>
+                    Password
+                  </h2>
+                  <div class="col-12 mb-4 mt-4">
+                    <form>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          OLD PASSWORD
+                        </label>
+                        <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Old Password" aria-describedby="emailHelp" />
+                      </div>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          NEW PASSWORD
+                        </label>
+                        <input type="password" class="form-control" id="exampleInputEmail1" placeholder="New Password" aria-describedby="emailHelp" />
+                      </div>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                          REPEAT NEW PASSWORD
+                        </label>
+                        <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Repeat New Password" aria-describedby="emailHelp" />
+                      </div>
+                      <button type="submit" class="btn btn-primary" style={{ backgroundColor: '#A08336', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', textAlign: 'center', border: 'black', float: 'right' }}>
+                        Save Changes
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </section>
+            </Card.Body>
+          </Card>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default AkunDetails;
