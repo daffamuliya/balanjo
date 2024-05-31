58815e52d10f4480e81053e045df4c888b705e4d
daffamuliya <daffamuliya15@gmail.com>
2024-06-01 05:48:17 +0700

    .

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,67 +30,69 @@ function Login() {
 
   return (
     <MDBContainer fluid>
-      <MDBRow className="d-flex justify-content-center align-items-center h-100">
-        <MDBCol col="12">
-          <MDBCard className="bg-white my-5 mx-auto" style={{ borderRadius: '10px', maxWidth: '644px' }}>
-            <MDBCardBody className="p-5 w-100 d-flex flex-column ">
-              <h2 className="fw mb-2 text-center" style={{ fontSize: '24px', fontWeight: 'bold' }}>
-                Login
-              </h2>
-              <form onSubmit={Auth}>
-                {isError && <p className="text-center">{message}</p>}
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Email
-                </p>
-                <MDBInput
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your email address"
-                  className="focus-ring focus-ring-light "
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Password
-                </p>
-                <MDBInput
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="password"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your password"
-                  className="focus-ring focus-ring-light"
-                  autoComplete="new-password"
-                />
-                <MDBRow className="d-flex justify-content-center align-items-center text-center">
-                  <MDBBtn type="submit" size="lg" onClick={Auth} style={{ backgroundColor: 'black', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'semi-bold', textAlign: 'center', border: 'black' }}>
-                    Sign In
+      <section className="login">
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard className="bg-white my-5 mx-auto" style={{ borderRadius: '10px', maxWidth: '644px' }}>
+              <MDBCardBody className="p-5 w-100 d-flex flex-column ">
+                <h2 className="fw mb-2 text-center" style={{ fontSize: '24px', fontWeight: 'bold' }}>
+                  Login
+                </h2>
+                <form onSubmit={Auth}>
+                  {isError && <p className="text-center">{message}</p>}
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Email
+                  </p>
+                  <MDBInput
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="email"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your email address"
+                    className="focus-ring focus-ring-light "
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Password
+                  </p>
+                  <MDBInput
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="password"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your password"
+                    className="focus-ring focus-ring-light"
+                    autoComplete="new-password"
+                  />
+                  <MDBRow className="d-flex justify-content-center align-items-center text-center">
+                    <MDBBtn type="submit" size="lg" onClick={Auth} style={{ backgroundColor: 'black', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'semi-bold', textAlign: 'center', border: 'black' }}>
+                      Sign In
+                    </MDBBtn>
+                  </MDBRow>
+                </form>
+                <br></br>
+                <hr className="my-4" />
+                <br></br>
+                <div className="d-flex justify-content-center">
+                  <p className="text-black-100 me-3 mt-2" style={{ fontSize: '14px' }}>
+                    Don't have an account?
+                  </p>
+                  <MDBBtn className="w-100" href="/register" size="lg" style={{ backgroundColor: 'white', color: 'black', borderColor: '#7E7E7E', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'bold' }}>
+                    <MDBIcon fab icon="google" />
+                    Register
                   </MDBBtn>
-                </MDBRow>
-              </form>
-              <br></br>
-              <hr className="my-4" />
-              <br></br>
-              <div className="d-flex justify-content-center">
-                <p className="text-black-100 me-3 mt-2" style={{ fontSize: '14px' }}>
-                  Don't have an account?
-                </p>
-                <MDBBtn className="w-100" href="/register" size="lg" style={{ backgroundColor: 'white', color: 'black', borderColor: '#7E7E7E', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'bold' }}>
-                  <MDBIcon fab icon="google" />
-                  Register
-                </MDBBtn>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </section>
     </MDBContainer>
   );
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -40,127 +40,129 @@ function Register() {
   };
   return (
     <MDBContainer fluid>
-      <MDBRow className="d-flex justify-content-center align-items-center h-100">
-        <MDBCol col="12">
-          <MDBCard className="bg-white my-5 mx-auto" style={{ borderRadius: '10px', maxWidth: '644px' }}>
-            <MDBCardBody className="p-5 w-100 d-flex flex-column ">
-              <h2 className="fw mb-2 text-center" style={{ fontSize: '24px', fontWeight: 'bold' }}>
-                Register
-              </h2>
-              <form onSubmit={Register}>
-                <p className="text-center">{msg}</p>
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Full Name
-                </p>
-                <MDBInput
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your full name"
-                  className="focus-ring focus-ring-light "
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Username
-                </p>
-                <MDBInput
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your display name"
-                  className="focus-ring focus-ring-light "
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Phone
-                </p>
-                <MDBInput
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your phone number"
-                  className="focus-ring focus-ring-light "
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Email
-                </p>
-                <MDBInput
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your email address"
-                  className="focus-ring focus-ring-light "
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Password
-                </p>
-                <MDBInput
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="password"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your password"
-                  className="focus-ring focus-ring-light"
-                  autoComplete="new-password"
-                />
-                <p className="text-black-100" style={{ fontSize: '16px' }}>
-                  Re-type Password
-                </p>
-                <MDBInput
-                  value={confPassword}
-                  onChange={(e) => setConfPassword(e.target.value)}
-                  wrapperClass="mb-4 w-100"
-                  id="formControlLg"
-                  type="password"
-                  size="lg"
-                  style={{ fontSize: '16px' }}
-                  placeholder="your password"
-                  className="focus-ring focus-ring-light"
-                  autoComplete="new-password"
-                />
-                <MDBRow className="d-flex justify-content-center align-items-center text-center">
-                  <MDBBtn type="submit" size="lg" onClick={Register} style={{ backgroundColor: 'black', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'semi-bold', textAlign: 'center', border: 'black' }}>
-                    Create Account
+      <section className="regist">
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard className="bg-white my-5 mx-auto" style={{ borderRadius: '10px', maxWidth: '644px' }}>
+              <MDBCardBody className="p-5 w-100 d-flex flex-column ">
+                <h2 className="fw mb-2 text-center" style={{ fontSize: '24px', fontWeight: 'bold' }}>
+                  Register
+                </h2>
+                <form onSubmit={Register}>
+                  <p className="text-center">{msg}</p>
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Full Name
+                  </p>
+                  <MDBInput
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="email"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your full name"
+                    className="focus-ring focus-ring-light "
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Username
+                  </p>
+                  <MDBInput
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="email"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your display name"
+                    className="focus-ring focus-ring-light "
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Phone
+                  </p>
+                  <MDBInput
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="email"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your phone number"
+                    className="focus-ring focus-ring-light "
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Email
+                  </p>
+                  <MDBInput
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="email"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your email address"
+                    className="focus-ring focus-ring-light "
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Password
+                  </p>
+                  <MDBInput
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="password"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your password"
+                    className="focus-ring focus-ring-light"
+                    autoComplete="new-password"
+                  />
+                  <p className="text-black-100" style={{ fontSize: '16px' }}>
+                    Re-type Password
+                  </p>
+                  <MDBInput
+                    value={confPassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
+                    wrapperClass="mb-4 w-100"
+                    id="formControlLg"
+                    type="password"
+                    size="lg"
+                    style={{ fontSize: '16px' }}
+                    placeholder="your password"
+                    className="focus-ring focus-ring-light"
+                    autoComplete="new-password"
+                  />
+                  <MDBRow className="d-flex justify-content-center align-items-center text-center">
+                    <MDBBtn type="submit" size="lg" onClick={Register} style={{ backgroundColor: 'black', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'semi-bold', textAlign: 'center', border: 'black' }}>
+                      Create Account
+                    </MDBBtn>
+                  </MDBRow>
+                </form>
+                <br></br>
+                <hr className="my-4" />
+                <br></br>
+                <div className="d-flex justify-content-center">
+                  <p className="text-black-100 me-3 mt-2" style={{ fontSize: '14px' }}>
+                    Already have an account?
+                  </p>
+                  <MDBBtn className="w-100" size="lg" href="/login" style={{ backgroundColor: 'white', color: 'black', borderColor: '#7E7E7E', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'bold' }}>
+                    <MDBIcon fab icon="google" />
+                    Sign in
                   </MDBBtn>
-                </MDBRow>
-              </form>
-              <br></br>
-              <hr className="my-4" />
-              <br></br>
-              <div className="d-flex justify-content-center">
-                <p className="text-black-100 me-3 mt-2" style={{ fontSize: '14px' }}>
-                  Already have an account?
-                </p>
-                <MDBBtn className="w-100" size="lg" href="/login" style={{ backgroundColor: 'white', color: 'black', borderColor: '#7E7E7E', fontSize: '16px', maxWidth: '158px', maxHeight: '42px', fontWeight: 'bold' }}>
-                  <MDBIcon fab icon="google" />
-                  Sign in
-                </MDBBtn>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </section>
     </MDBContainer>
   );
 }
